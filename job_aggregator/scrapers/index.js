1c16f687ae658cda50b3d74e8e2c9eefd8c526b3
rslonee <rslonee@gmail.com>
2025-05-12 11:03:27 -0500

    Update index.js

--- job_aggregator/scrapers/index.js
+++ job_aggregator/scrapers/index.js
@@ -1,6 +1,6 @@
 const scrapers = {
   lever: require('./lever'),
-  greenhouse: require('./greenhouse'),
-  workday: require('./workday'),
+  //greenhouse: require('./greenhouse'),
+  //workday: require('./workday'),
   ashby: require('./ashby'),   // ← now points at your JS file
 };
