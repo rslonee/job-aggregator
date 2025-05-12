0b06be611c0b7b7cd7621f60137f2a541dc45938
rslonee <rslonee@gmail.com>
2025-05-12 10:52:45 -0500

    Create index.js

---
+++ job_aggregator/scrapers/index.js
@@ -0,0 +1,7 @@
+// job-aggregator/scrapers/index.js
+const scrapers = {
+  lever: require('./lever'),
+  greenhouse: require('./greenhouse'),
+  workday: require('./workday'),
+  ashby: require('./ashby'),           // ← add this line
+};
