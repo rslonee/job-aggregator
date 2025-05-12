10b5cd986dba7a1e45da9b47c6488a2c453b9492
rslonee <rslonee@gmail.com>
2025-05-12 11:23:45 -0500

    Delete job_aggregator/scrapers/index.js

--- job_aggregator/scrapers/index.js
+++
@@ -1,6 +0,0 @@
-const scrapers = {
-  lever: require('./lever'),
-  //greenhouse: require('./greenhouse'),
-  //workday: require('./workday'),
-  ashby: require('./ashby'),   // ← now points at your JS file
-};
