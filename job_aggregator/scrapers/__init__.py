8aade900c0b5d7f87a962c7bc5840ed3d741c474
rslonee <rslonee@gmail.com>
2025-05-12 00:23:03 -0500

    Update __init__.py

--- job_aggregator/scrapers/__init__.py
+++ job_aggregator/scrapers/__init__.py
@@ -9,11 +9,6 @@ SCRAPERS = {
 }
 
 def get_scraper(scraper_type: str, site: dict):
-    """
-    Returns an instance of the scraper class for this site.
-      - scraper_type: string key in SCRAPERS
-      - site: full site dict from load_sites_config()
-    """
     key = scraper_type.strip().lower()
     cls = SCRAPERS.get(key)
     if not cls:
